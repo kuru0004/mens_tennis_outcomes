a5f958435a798ba779b001857881a02c805a78c0
kuru0004 <kurup.arjun@gmail.com>
2018-08-11 10:13:59 -0500

    give better name for Explore/Clean Jupyter notebook, intiate predict .py file

---
+++ tennis_predict.py
@@ -0,0 +1,29 @@
+
+
+"""
+Pseudo code:
+
+To train:
+
+given1 = match_date
+given2 = player_1_name
+given3 = player_2_name
+given4 = surface 
+## posit : clay = 0.5 similar to grass (sseems should be higher)
+## posit : hard = 0.8 similar to grss (seems right)
+## posit : hard = 0.7 similar to clay 
+### note: a players winning percentage with be deceptively low (if they lose a previous match, they might have won a subsequent match according to their abilities)
+...Therefore, should I ascribe a winning percentage?
+
+time_weights = 
+for match1 in sorted(recent_matches1 asceiding=False):
+	#
+	for stat in stat_list: 
+
+All this in a nutshell:
+A player is the 'total' of their *relevant* stats
+	--stat subset
+	--stat falloff based on time
+This valuation of player can be put in model
+
+"""
