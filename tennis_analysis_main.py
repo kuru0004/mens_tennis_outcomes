f2c613b365b2a3f52476d08d1696390d2d1c6cd2
kuru0004 <kurup.arjun@gmail.com>
2018-08-11 21:05:35 -0500

    create .py file to run analysis from

---
+++ tennis_analysis_main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+player1 = input('Enter name of player1: ')
+player2 = input('Enter name of player2: ')
+date = pd.to_datetime(input('Enter date of match: '))
+surface = input('Please enter surface: options are Grass,
+	Clay, Hard, and Carpet')
+
+
+def run_analysis(player1,player2,surface, date):
+	pass
+
+
+
